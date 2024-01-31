c01df0b3ec3eb085ed4c6e7114fb84d7d1bd5a28
Guilherme Lettmann Penha <134228041+guilettmann@users.noreply.github.com>
2024-01-31 11:05:00 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <Arduino.h>
+#include "driver/gpio.h"
+
+// put function declarations here:
+#define BUTTON GPIO_NUM_15
+bool RTD = false;
+
+void setup() {
+  // put your setup code here, to run once:
+   pinMode (BUTTON, INPUT_PULLDOWN);
+   Serial.begin(115200);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalRead(BUTTON);
+  if (digitalRead(BUTTON)==HIGH) {
+      RTD = HIGH;
+    }
+  else { RTD = LOW;
+    }
+
+
+}
